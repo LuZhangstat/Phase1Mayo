ff6fbcd52c591cfd7613602e435d72b3a9f94b1b
Lu Zhang <lu.zhang@ucla.edu>
2019-01-29 20:49:47 -0800

    modify the help document

--- R/RunPRMD.R
+++ R/RunPRMD.R
@@ -140,37 +140,38 @@ RunPRMD <-function(seed = 1234, patlist, patID_act = NULL,
   #'   \code{\link{SimPRMD}}.
   #'
   #' @examples
-  #'# data("patlist_sim")
-  #'# # check the whole dataset by function patlist.display
-  #'# patlist.display(patlist_sim, n.dose = 6, n.cycle = 6)
-  #'#
-  #'# # When we pick the records before 6th cohort enrolled in the study
-  #'# L <- length(patlist_sim$PatID)
-  #'# patlist <- lapply(patlist_sim, function(a){a <- a[-(44:L)]})
-  #'# patlist.display(patlist, n.dose = 6, n.cycle = 6)
+  #' data("patlist_sim")
+  #' # check the whole dataset by function patlist.display
+  #' patlist.display(patlist_sim, n.dose = 6, n.cycle = 6)
   #'
-  #'# #The table shows the current patient in the trial. Now record the active
-  #'# #patient ID and records as follows
+  #' # When we pick the records before 6th cohort enrolled in the study
+  #' L <- length(patlist_sim$PatID)
+  #' patlist <- lapply(patlist_sim, function(a){a <- a[-(44:L)]})
+  #' patlist.display(patlist, n.dose = 6, n.cycle = 6)
   #'
-  #'# patID_act <- c("cohort1subject1", "cohort1subject2", "cohort1subject3",
-  #'#                "cohort2subject1", "cohort2subject2", "cohort2subject3",
-  #'#                "cohort3subject2", "cohort3subject3",
-  #'#                "cohort4subject1", "cohort4subject2", "cohort4subject3",
-  #'#                "cohort5subject1", "cohort5subject2", "cohort5subject3")
-  #'# cycle_act <- c(5, 5, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1 ,1)
-  #'# dose_act <- c(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4)
-  #'# dlt_act <- c(0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0)
-  #'#
-  #'# test <- RunPRMD(patlist = patlist, patID_act = patID_act,
-  #'#                 cycle_act = cycle_act, dose_act = dose_act,
-  #'#                 dlt_act = dlt_act, trialSize = 36, chSize = 3,
-  #'#                 effcy.flag = TRUE, ICD.flag = TRUE, DLT.drop.flag = TRUE,
-  #'#                 IED.flag = TRUE, ICD_thrd = 0.3)
-  #'#
-  #'# summary(test)
-  #'# plot(test)
-  #'# plot(test, select_cycle = 1:2)
+  #' #The table shows the current patient in the trial. Now record the active
+  #' #patient ID and records as follows
   #'
+  #' patID_act <- c("cohort1subject1", "cohort1subject2", "cohort1subject3",
+  #'                "cohort2subject1", "cohort2subject2", "cohort2subject3",
+  #'                "cohort3subject2", "cohort3subject3",
+  #'                "cohort4subject1", "cohort4subject2", "cohort4subject3",
+  #'                "cohort5subject1", "cohort5subject2", "cohort5subject3")
+  #' cycle_act <- c(5, 5, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1 ,1)
+  #' dose_act <- c(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4)
+  #' dlt_act <- c(0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0)
+  #'
+  #'\donttest{
+  #' test <- RunPRMD(patlist = patlist, patID_act = patID_act,
+  #'                 cycle_act = cycle_act, dose_act = dose_act,
+  #'                 dlt_act = dlt_act, trialSize = 36, chSize = 3,
+  #'                 effcy.flag = TRUE, ICD.flag = TRUE, DLT.drop.flag = TRUE,
+  #'                 IED.flag = TRUE, ICD_thrd = 0.3)
+  #'
+  #' summary(test)
+  #' plot(test)
+  #' plot(test, select_cycle = 1:2)
+  #'}
   #'
   #' @import coda
   #' @importFrom arrayhelpers vec2array
--- R/SimPRMD.R
+++ R/SimPRMD.R
@@ -199,40 +199,42 @@ SimPRMD <-function(seed = 1234, numTrials = 100, doses = 1:6, cycles = 1:6,
   #'
   #'
   #' @examples
-  #'# data("prob")      # load prob.RData from package phaseI, Details see "?prob"
-  #'# data("eff")       # load eff.RData from package phaseI. Details see "?eff"
+  #' data("prob")      # load prob.RData from package phaseI, Details see "?prob"
+  #' data("eff")       # load eff.RData from package phaseI. Details see "?eff"
   #'
-  #'# eff.structure = eff$Dose_Cycle_Meff[2, 2, , ]
-  #'# eff.Sigma = eff$Sigma
-  #'# eff.sd_trans = eff$sd_trans
+  #' eff.structure = eff$Dose_Cycle_Meff[2, 2, , ]
+  #' eff.Sigma = eff$Sigma
+  #' eff.sd_trans = eff$sd_trans
   #'
-  #'# wm <- matrix(c(0, 0.5, 0.75, 1, 1.5,
-  #'#                0, 0.5, 0.75, 1, 1.5,
-  #'#                0, 0, 0, 0.5, 1),
-  #'#              byrow = TRUE, ncol
-  #'#               = 5)                          # weighted matrix for toxicity matrix
-  #'#                                             # nrow = No.of type; ncol = No. of grade
-  #'# toxmax <- 2.5
-  #'# tox.matrix <- prob["MTD4", "flat", , , , ]
-  #'#
-  #'#
-  #'# #------- a flat dose-toxicity, dose-efficacy, cycle-efficacy pattern------#
-  #'# simul1 <- SimPRMD(numTrials = 1, tox.matrix = tox.matrix,
-  #'#                   eff.structure = eff.structure, eff.Sigma = eff.Sigma,
-  #'#                   eff.sd_trans = eff.sd_trans, wm = wm, toxmax = toxmax,
-  #'#                   trialSize = 36)
-  #'#
-  #'# #------- a flat dose-toxicity pattern model ------#
-  #'# simul2 <- SimPRMD(numTrials = 1, toxtype = c("H", "L", "M"),
-  #'#                   intercept.alpha = c(1.9, 2.3, 2.6, 3.1),
-  #'#                   coef.beta = c(-0.3, -0.2, -0.25),
-  #'#                   cycle.gamma = 0, tox.target = 0.23,
-  #'#                   thrd1 = 0.23, thrd2 = 0.23, p_tox1 = 0.2, p_tox2 = 0.2,
-  #'#                   ICD.flag = FALSE, IED.flag = FALSE, effcy.flag = TRUE)
-  #'#
-  #'# summary(simul2)
-  #'# plot(simul2)
+  #' wm <- matrix(c(0, 0.5, 0.75, 1, 1.5,
+  #'                0, 0.5, 0.75, 1, 1.5,
+  #'                0, 0, 0, 0.5, 1),
+  #'              byrow = TRUE, ncol
+  #'               = 5)                          # weighted matrix for toxicity matrix
+  #'                                             # nrow = No.of type; ncol = No. of grade
+  #' toxmax <- 2.5
+  #' tox.matrix <- prob["MTD4", "flat", , , , ]
   #'
+  #'
+  #' #------- a flat dose-toxicity, dose-efficacy, cycle-efficacy pattern------#
+  #' \donttest{
+  #' simul1 <- SimPRMD(numTrials = 1, tox.matrix = tox.matrix,
+  #'                   eff.structure = eff.structure, eff.Sigma = eff.Sigma,
+  #'                   eff.sd_trans = eff.sd_trans, wm = wm, toxmax = toxmax,
+  #'                   trialSize = 36)
+  #' }
+  #' #------- a flat dose-toxicity pattern model ------#
+  #' \donttest{
+  #' simul2 <- SimPRMD(numTrials = 1, toxtype = c("H", "L", "M"),
+  #'                   intercept.alpha = c(1.9, 2.3, 2.6, 3.1),
+  #'                   coef.beta = c(-0.3, -0.2, -0.25),
+  #'                   cycle.gamma = 0, tox.target = 0.23,
+  #'                   thrd1 = 0.23, thrd2 = 0.23, p_tox1 = 0.2, p_tox2 = 0.2,
+  #'                   ICD.flag = FALSE, IED.flag = FALSE, effcy.flag = TRUE)
+  #'
+  #' summary(simul2)
+  #' plot(simul2)
+  #' }
   #'
   #' @import coda
   #' @importFrom arrayhelpers vec2array
